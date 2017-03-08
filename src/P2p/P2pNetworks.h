1802770585c3e6bc7ba17c1d55fc63ab38af8f5a
IdentityProject <xid.identity.project@gmail.com>
2017-03-08 17:05:53 -0600

    Moved Identity to GitHub

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x52, 0x4F, 0x52, 0x13, 0x66, 0x72, 0x77, 0x76, 0x27, 0x76, 0x77, 0x77, 0x56, 0x5D, 0x02, 0x02 } };
+}
